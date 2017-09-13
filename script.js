e98cecea187715705c8dd491c5dbad809c86c469
Sergey Shapirenko <sshapirenko@gmail.com>
2017-09-13 15:05:57 +0700

    monir fixes

--- script.js
+++ script.js
@@ -1,125 +1,149 @@
-document.addEventListener("DOMContentLoaded", function(){
+/* eslint no-new:0 */
+/* global document, Vue, _, alert */
+document.addEventListener('DOMContentLoaded', () => {
 	new Vue({
-		el:'#game',
-		data: function(){
-			return{	
-				dirX:1,
-				dirY:0,
-				cellSize:20,
-				field:[],
-				intervalId:null,
-				enable:false,
-				mapWidth:40,
-				mapHeight:30,
-				coordX:3,
-				coordY:0,
-				snakeBody:[{ x:0, y:0 }, { x:1, y:0 }, { x:2, y:0 }, { x:3, y:0 }],
-				foodX:Math.floor(Math.random() * 39) + 1,
-				foodY:Math.floor(Math.random() * 29) + 1,
-				score:0,
-				endGame:false
-			}
-		},
-		ready (){
-
-			/*создание анимации*/
-
-			this.intervalId = setInterval(this.gameStart, 110);
-			
-			/*генерация поля*/
-
-			for (let y = 1; y <= this.mapHeight; y ++) {
-				for (let x = 1; x <= this.mapWidth; x ++) {
-					this.field.push({
-						x:(x - 1),
-						y:(y - 1)
-					})
-				};
+		el: '#game',
+		data() {
+			return {
+				dirX: 1,
+				dirY: 0,
+				cellSize: 20,
+				field: [],
+				intervalId: null,
+				enable: false,
+				mapWidth: 40,
+				mapHeight: 30,
+				coordX: 3,
+				coordY: 0,
+				snakeBody: [
+					{ x: 0, y: 0 },
+					{ x: 1, y: 0 },
+					{ x: 2, y: 0 },
+					{ x: 3, y: 0 }
+				],
+				foodX: Math.floor(Math.random() * 39) + 1,
+				foodY: Math.floor(Math.random() * 29) + 1,
+				score: 0,
+				endGame: false
 			};
 		},
-		methods:{
-			down () {
-				if(this.dirX == 0 && this.dirY == -1){return}
+		ready() {
+			/* создание анимации */
+
+			this.intervalId = setInterval(this.gameStart, 110);
+
+			/* генерация поля */
+
+			for (let y = 1; y <= this.mapHeight; y += 1) {
+				for (let x = 1; x <= this.mapWidth; x += 1) {
+					this.field.push({
+						x: x - 1,
+						y: y - 1
+					});
+				}
+			}
+		},
+		methods: {
+			down() {
+				if (this.dirX === 0 && this.dirY === -1) {
+					return;
+				}
 				this.dirX = 0;
 				this.dirY = 1;
 			},
-			up () {
-				if(this.dirX == 0 && this.dirY == 1){return}
+			up() {
+				if (this.dirX === 0 && this.dirY === 1) {
+					return;
+				}
 				this.dirX = 0;
 				this.dirY = -1;
 			},
-			left () {
-				if(this.dirX == 1 && this.dirY == 0){return}
+			left() {
+				if (this.dirX === 1 && this.dirY === 0) {
+					return;
+				}
 				this.dirX = -1;
 				this.dirY = 0;
 			},
-			right () {
-				if(this.dirX == -1 && this.dirY == 0){return}
+			right() {
+				if (this.dirX === -1 && this.dirY === 0) {
+					return;
+				}
 				this.dirX = 1;
 				this.dirY = 0;
 			},
-			start () {
-				if(this.endGame == false){
+			start() {
+				if (this.endGame === false) {
 					this.intervalId = null;
-					this.enable = true;					
+					this.enable = true;
 				}
 			},
-			reset () {
-				this.enable = false;				
+			reset() {
+				this.enable = false;
 				this.dirX = 1;
 				this.dirY = 0;
 				this.score = 0;
 				this.coordX = 3;
 				this.coordY = 0;
 				this.snakeBody = [];
-				this.snakeBody = [{ x:0, y:0 }, { x:1, y:0 }, { x:2, y:0 }, { x:3, y:0 }];
+				this.snakeBody = [
+					{ x: 0, y: 0 },
+					{ x: 1, y: 0 },
+					{ x: 2, y: 0 },
+					{ x: 3, y: 0 }
+				];
 				this.foodX = Math.floor(Math.random() * 39) + 1;
 				this.foodY = Math.floor(Math.random() * 29) + 1;
 				this.endGame = false;
 			},
-			gameStart () {
+			gameStart() {
 				const self = this;
-				if(!this.enable){return};
-				this.coordX += this.dirX;
-				this.coordY += this.dirY;	
-				this.snakeBody = _.rest(this.snakeBody);
-				if(_.some(this.snakeBody, function(item){
-					return self.coordX == item.x && self.coordY == item.y;
-				})){
-					this.enable = false;					
-					alert('Game over');
-					clearInterval(self.intervalId);
-					this.endGame = true;
+				if (!this.enable) {
 					return;
-				};
-				if(this.coordX == this.mapWidth
-					|| this.coordY == this.mapHeight
-					|| this.coordX < 0
-					|| this.coordY < 0
+				}
+				this.coordX += this.dirX;
+				this.coordY += this.dirY;
+				this.snakeBody = _.rest(this.snakeBody);
+				if (
+					_.some(this.snakeBody, (item) => {
+						return self.coordX === item.x && self.coordY === item.y;
+					})
 				) {
 					this.enable = false;
 					alert('Game over');
 					clearInterval(self.intervalId);
 					this.endGame = true;
 					return;
-				};
-				if(this.coordX == this.foodX && this.coordY == this.foodY){
+				}
+				if (
+					this.coordX === this.mapWidth ||
+					this.coordY === this.mapHeight ||
+					this.coordX < 0 ||
+					this.coordY < 0
+				) {
+					this.enable = false;
+					alert('Game over');
+					clearInterval(self.intervalId);
+					this.endGame = true;
+					return;
+				}
+				if (this.coordX === this.foodX && this.coordY === this.foodY) {
 					this.score += 1;
 					this.foodX = Math.floor(Math.random() * 39) + 1;
 					this.foodY = Math.floor(Math.random() * 29) + 1;
-					
-					/*добавление длины змеи*/
+
+					/* добавление длины змеи */
 
 					this.snakeBody.push({
-						x:this.coordX,
-						y:this.coordY
+						x: this.coordX,
+						y: this.coordY
 					});
-				};
+				}
 				this.snakeBody.push({
-					x:this.coordX,
-					y:this.coordY
+					x: this.coordX,
+					y: this.coordY
 				});
 			}
-		},
-	})
-})
+		}
+	});
+});
